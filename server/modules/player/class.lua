e125b2fa9f269b6fe618c1117d41bd0f6871e565
JericoFX <56100605+JericoFX@users.noreply.github.com>
2024-04-29 11:10:24 -0300

    asd

--- server/modules/player/class.lua
+++ server/modules/player/class.lua
@@ -2,4 +2,12 @@
     Set Player Data.
     Set Player Metadata
     Get Data
+
+    Data Player:
+        * First Name
+        * Last Name
+        * metadata
+            * Health
+            * Armor
+            * 
 ]]
