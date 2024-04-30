3220477f1df29590b2a7ce77933a12e2a5087787
JericoFX <56100605+JericoFX@users.noreply.github.com>
2024-04-30 13:38:34 -0300

    asddas

--- server/modules/player/class.lua
+++ server/modules/player/class.lua
@@ -1,3 +1,4 @@
+local db = lib.load("server.modules.player.db")
 --[[
     Set Player Data.
     Set Player Metadata
@@ -11,3 +12,63 @@
             * Armor
             * 
 ]]
+
+local Player = lib.class("Player")
+
+function Player:constructor(license,source,newData)
+    self.license = license
+    self.source = source
+    self.private.metadata = nil
+    self.private.charinfo = nil
+    self.location = nil
+    self.data = {}
+    self.newData = newData or nil
+    return self
+end
+
+function Player:getInformation()
+   self.data =  db.getPlayerInformation(self.license)
+   if self.data then
+    self.name = data.name
+    self.private.metadata = json.decode(data.metadata)
+    self.private.charinfo = json.decode(data.charinfo)
+    self.location = json.decode(data.charinfo)
+    return true
+   else
+    return self:createNewCharacter()
+   end
+end
+
+function Player:createNewCharacter()
+    self.private.charinfo = {
+        firstname = self.newData.firstname
+        lastname = self.newData.lastname
+        date = self.newData.date
+        id = self.newData.id
+    }
+    Wait(10)
+    self.private.metadata = {
+        hunger = 100
+        thirst = 100
+        stress = 0
+        isdead = false
+        inlaststand = false
+        armor = 0
+        ishandcuffed = false
+        mission = {}
+        group = {}
+        effect = self.newData.effect
+    }
+    return true
+end
+
+function Player:setMetadata(key,value)
+    if not self.private.metadata[key] then return end
+     self.private.metadata[key] = value
+    return true
+end
+
+function Player:getMetadata(key)
+    if not self.private.metadata[key] then return end
+    return self.private.metadata[key]
+end
